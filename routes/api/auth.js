e3038fc9a84f3d96fee480dd52be5522dd0f29b9
Luc Audemard <luc@Lucs-MacBook-Pro.local>
2019-08-27 01:54:56 +0100

    Added routes and users model

---
+++ routes/api/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+
+// @route   Get api/auth
+// @desc    Test route
+// @access Public
+
+router.get('/', auth, (req, res) => res.send('Auth route'));
+
+module.exports = router;
