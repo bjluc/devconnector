5330385902d1bbdbc75890bfdbac53e9afc76aa8
Luc Audemard <luc@Lucs-MacBook-Pro.local>
2019-09-01 18:53:58 +0100

    profile education, github and experience

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -64,7 +64,7 @@ export const getProfileById = userId => async dispatch => {
 };
 
 // Get Github repos
-export const getGithubTepos = username => async dispatch => {
+export const getGithubRepos = username => async dispatch => {
   try {
     const res = await axios.get(`/api/profile/github/${username}`);
 
@@ -79,7 +79,6 @@ export const getGithubTepos = username => async dispatch => {
     });
   }
 };
-
 // Create or update profile
 export const createProfile = (
   formData,
